30c1ce15f28e9494b649a51b3a9e03a08b02d4a4
nikkii03 <75238435+nikkii03@users.noreply.github.com>
2022-11-06 08:29:25 +0530

    Add files via upload

---
+++ frontend/function.js
@@ -0,0 +1,73 @@
+$(document).ready(function () {
+ 
+    'use strict';
+ 
+    var slide  = $('.slide'),
+        slideAelements = $('.slide-a-child'),
+        slideBelements = $('.slide-b-child'),
+        slideCelements = $('.slide-c-child'),
+        slideDelements = $('.slide-d-child');
+        
+ 
+ 
+ 
+    slide.each(function (i) {
+        if (i < 3) {
+            setTimeout(function () {
+                slide.eq(i).fadeOut();
+            }, 5000 * (i + 1));
+        }
+    });
+ 
+ 
+    function animateSlideA() {
+      slideAelements.each(function (i) {
+          setTimeout(function () {
+              slideAelements.eq(i).addClass('is-visible');
+          }, 300 * (i + 1));
+      });
+    }
+ 
+    function animateSlideB() {
+      slideBelements.each(function (i) {
+          setTimeout(function () {
+              slideBelements.eq(i).addClass('is-visible');
+          }, 300 * (i + 1));
+      });
+    }
+ 
+    function animateSlideC() {
+     slideCelements.each(function (i) {
+         setTimeout(function () {
+             slideCelements.eq(i).addClass('is-visible');
+         }, 150 * (i + 1));
+     });
+   }
+
+   function animateSlideD() {
+     slideDelements.each(function (i) {
+         setTimeout(function () {
+             slideDelements.eq(i).addClass('is-visible');
+         }, 300 * (i + 1));
+     });
+   }
+
+   animateSlideA();
+
+   setTimeout(function () {
+      animateSlideB();
+   }, 5000);
+
+   setTimeout(function () {
+      animateSlideC();
+   }, 10000);
+
+   setTimeout(function () {
+      animateSlideD();
+   }, 15000);
+
+
+  replay.on('click', function () {
+    location.landing.html(true);
+  });
+});
